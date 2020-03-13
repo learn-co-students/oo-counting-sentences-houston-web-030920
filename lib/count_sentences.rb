9b3aef7fc67fb146c16b8b888cb2668b49160fe0
melkilic <melike180400@gmail.com>
2020-03-13 08:45:18 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,34 @@
-require 'pry'
+
 
 class String
-
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else 
+      return false
+    end
   end
 
   def question?
-
+     if self.end_with?("?")
+      return true
+    else 
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else 
+      return false
+    end
   end
 
   def count_sentences
+  #  my_array = ["!", "?", "."]
+
+   my_array = self.split(/[!.?]/).select {|string| string != ""}.length
 
   end
 end
